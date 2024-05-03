afaab819bc96baae4ab21a6b466a4cae238b8b09
“Muhammad <sakhran.t@gmail.com>
2024-05-03 19:57:52 +0700

    WindowTestBranch Before Creating Two Windows But With Templates Too

--- src/index.js
+++ src/index.js
@@ -1,51 +1,44 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+console.log("Hello World")
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
+const electron = require("electron")
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+
+let win; //Creates a variable 'win', which is currently undefined (will be defined more below)
+
+//Basic function that opens a Window
+function createWindow() {
+    win = new BrowserWindow(); //Create new instance of BrowserWindow, name it win
+
+    win.loadURL(url.format({ //Basically tells us what win is going to be doing, it will loadURL
+        pathname: path.join(__dirname, 'index.html'), //Tells them where is it, __dirname is for when the .html file is in the same place as the .js file
+        protocol: 'file', //Protocol for local files
+        slashes: true //Whether slashes should be used in the file URL
+    }));
+
+
+    win.webContents.openDevTools(); // Opens DevTools in the BrowserWindow (optional)
+    win.on('closed', () => { //When all the windows are closed
+        win = null; //nullify it
+    });
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+//Once app is "ready", use the createWindow function to open the main UI window
+app.on('ready', createWindow);
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+//For Macintosh, if all the windows are closed, force quit the application
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+//For Macintosh, if there aren't any windows open, and the icon is still on the dock, it will recreate the window.
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+});
